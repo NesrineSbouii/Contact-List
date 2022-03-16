dc4efe09e508260456859c481ab85b624cb0c524
nesrine <nesrine.sboui@talantech.fr>
2022-03-16 11:54:08 +0100

    add project architercture

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config();
+app.set('view engine', 'ejs');
+//Routes
+app.use('/', require('./routes/login'));
+
+// Mongo DB conncetion
+const database = process.env.MONGOLAB_URI;
+
+mongoose.connect(database, {useUnifiedTopology: true, useNewUrlParser: true })
+.then(() => console.log('e don connect'))
+.catch(err => console.log(err));
+
+const PORT = process.env.PORT || 4111;
+app.listen(PORT, console.log("Server don start for port: " + PORT))
