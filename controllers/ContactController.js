321ef10f49b752447462e4919ce42b58a006741f
nesrine <nesrine.sboui@talantech.fr>
2022-03-18 18:54:42 +0100

    add xsl et pdf

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,16 +1,20 @@
+const excelJS = require("exceljs");
 const Contact = require("../models/Contact");
 const qrCode = require('../dist/qrcode');
+var fs = require('fs');
+var pdf = require('html-pdf');
+
 const ContactFormView = (req, res) => {
   res.render("contactFormAdd", {
   });
 };
 
-const QrCodeView =  (req, res) => {
+const QrCodeView = (req, res) => {
   Contact.findById(req.params.id).then((contact) => {
-    if(contact){
+    if (contact) {
       console.log(contact.qrcode);
       res.render("qrcode", {
-        qrcode: contact.qrcode 
+        qrcode: contact.qrcode
       });
     }
   })
@@ -26,7 +30,7 @@ const AddContact = (req, res) => {
     console.log("Phone number not valid");
   } else {
     //Validation
-    var  testCard= {
+    var testCard = {
       firstName: username,
       cellPhone: phoneNumber
     }
@@ -35,7 +39,7 @@ const AddContact = (req, res) => {
       email,
       phoneNumber,
       notes,
-      qrcode: qrCode.createVCardQr(testCard, {typeNumber: 30, cellSize: 1})  
+      qrcode: qrCode.createVCardQr(testCard, { typeNumber: 30, cellSize: 1 })
     });
     newContact
       .save()
@@ -43,8 +47,93 @@ const AddContact = (req, res) => {
       .catch((err) => console.log(err));
   };
 }
+
+
+const exportContactExcel = async (req, res) => {
+  const workbook = new excelJS.Workbook();  // Create a new workbook
+  const worksheet = workbook.addWorksheet("My Contacts"); // New Worksheet
+  const path = "./files";  // Path to download excel
+  // Column for data in excel. key must match data key
+  worksheet.columns = [
+    { header: "ID", key: "counter", width: 10 },
+    { header: "User Name", key: "username", width: 10 },
+    { header: "Phone Number", key: "phoneNumber", width: 10 },
+    { header: "Email", key: "email", width: 10 },
+    { header: "Notes", key: "notes", width: 10 },
+  ];
+  // Looping through User data
+  let counter = 1;
+  Contact.find({}).then((contacts) => {
+    contacts.forEach((contact) => {
+      contact.counter = counter;
+      console.log({ email: contact.email, username: contact.username });
+      worksheet.addRow(contact); // Add data in worksheet
+      counter++;
+    })
+  });
+  // Making first line in excel bold
+  worksheet.getRow(1).eachCell((cell) => {
+    cell.font = { bold: true };
+  });
+  try {
+    const data = await workbook.xlsx.writeFile(`${path}/contacts.xlsx`)
+      .then(() => {
+        res.send({
+          status: "success",
+          message: "file successfully downloaded",
+          path: `${path}/contacts.xlsx`,
+        });
+      });
+  } catch (err) {
+    res.send({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
+
+const exportContactPdf = async (req, res) => {
+  var table = "";
+  table += "<table border='1' style='width:100%;word-break:break-word;'>";
+  table += "<tr>";
+  table += "<th > User Name </th>";
+  table += "<th > Email </th>";
+  table += "<th > Notes </th>";
+  table += "</tr>";
+
+  Contact.find({}).then((contacts) => {
+    contacts.forEach((contact) => {
+      table += "<tr>";
+      table += "<td>" + contact.username + "</td>";
+      table += "<td>" + contact.email + "</td>";
+      table += "<td>" + contact.notes + "</td>";
+      table += "</tr>";
+    })
+  });
+
+  table += "</table>";
+
+  var options = {
+    "format": "A4",
+    "orientation": "landscape",
+    "border": {
+      "top": "0.1in",
+    },
+    "timeout": "120000"
+  };
+  pdf.create(table, options).toFile('./files/contacts.pdf', function (err, result) {
+    if (err) return console.log(err);
+    res.send({
+      status: "success",
+      message: "file successfully downloaded",
+      path: `./files/contacts.pdf`,
+    });
+  });
+};
 module.exports = {
   ContactFormView,
   AddContact,
-  QrCodeView
+  QrCodeView,
+  exportContactExcel,
+  exportContactPdf
 };
--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
-const { ContactFormView, AddContact, QrCodeView} = require('../controllers/ContactController');
+const { ContactFormView, AddContact, QrCodeView, exportContactExcel, exportContactPdf} = require('../controllers/ContactController');
 const router = express.Router();
 router.get('/contactFormAdd', ContactFormView);
 router.post('/contactFormAdd', AddContact);
 router.get('/details/:id', QrCodeView);
+router.get("/downloadExcel", exportContactExcel);
+router.get("/downloadPdf", exportContactPdf);
 
 module.exports = router;    
